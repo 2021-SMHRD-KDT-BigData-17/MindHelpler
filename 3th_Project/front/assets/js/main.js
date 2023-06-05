6fc2b92915ca0e641b599590d5a12c1ddabb00c7
asefr01 <asefr01@gmail.com>
2023-06-05 10:42:02 +0900

    suervey 자바스크립트 코드 수정

--- 3th_Project/front/assets/js/main.js
+++ 3th_Project/front/assets/js/main.js
@@ -12,25 +12,33 @@ list.forEach((item) => item.addEventListener("mouseover", activeLink));
 /*=======================================================================*/
 let calcScrollValue = () => {
   let scrollProgress = document.getElementById("progress");
-  let progresssValue = document.getElementById("progress-value");
+  let progressValue = document.getElementById("progress-value");
+
+  if (!scrollProgress || !progressValue) {
+    return; // Exit the function if elements are not found
+  }
+
   let pos = document.documentElement.scrollTop;
   let calcHeight =
     document.documentElement.scrollHeight -
     document.documentElement.clientHeight;
   let scrollValue = Math.round((pos * 100) / calcHeight);
+
   if (pos > 100) {
     scrollProgress.style.display = "grid";
   } else {
     scrollProgress.style.display = "none";
   }
+
   scrollProgress.addEventListener("click", () => {
     document.documentElement.scrollTop = 0;
   });
+
   scrollProgress.style.background = `conic-gradient(#E3B172 ${scrollValue}%, #d7d7d7 ${scrollValue}% 100%)`;
 };
 
-window.onscroll = calcScrollValue;
-window.onload = calcScrollValue;
+window.addEventListener("scroll", calcScrollValue);
+window.addEventListener("load", calcScrollValue);
 
 /* ===================로그인==================================*/
 
