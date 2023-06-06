daaa60845db60db04263ed18c4be657c52dec83f
asefr01 <asefr01@gmail.com>
2023-06-07 08:44:19 +0900

    Front 상에서 플라스크 연동중

--- 3th_Project/front/app.py
+++ 3th_Project/front/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -24,7 +24,47 @@ df = load_dataset()
 
 @app.route('/')
 def home():
+    return redirect(url_for('index'))
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/survey')
+def survey():
+    return render_template('survey.html')
+
+
+@app.route('/emotion')
+def emotion():
+    return render_template('emotion.html')
+
+
+@app.route('/emotionDiary')
+def emotionDiary():
     return render_template('emotionDiary.html')
+
+
+@app.route('/userprofile')
+def userprofile():
+    return render_template('userprofile.html')
+
+
+@app.route('/Counselorfile')
+def Counselorfile():
+    return render_template('Counselorfile.html')
+
+
+@app.route('/login_Counselor')
+def login_Counselor():
+    return render_template('login_Counselor.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
 @app.route('/chatbot', methods=['POST'])
